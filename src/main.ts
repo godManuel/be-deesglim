c157a04b21d0d92332564bdd834e77882d3a5798
godManuel <emmanuelabraham375@gmail.com>
2026-07-06 09:52:54 -0700

    Added cors configuration

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
